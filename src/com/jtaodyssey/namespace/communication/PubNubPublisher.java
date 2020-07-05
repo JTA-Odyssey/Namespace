2bbb60dbfdcc2b097eef2e0f11f4eff4aa09d1db
Tucker Harvey <coopert.harvey@gmail.com>
2020-07-05 16:54:19 -0700

    PubNubPublisher has publish method

--- src/com/jtaodyssey/namespace/communication/PubNubPublisher.java
+++ src/com/jtaodyssey/namespace/communication/PubNubPublisher.java
@@ -1,14 +1,21 @@
 package com.jtaodyssey.namespace.communication;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.pubnub.api.PubNub;
+
 /**
  * This class should be called when ready to publish a method to pubnub
  */
 public final class PubNubPublisher {
     private volatile static PubNubPublisher publisher = null;
+    private final PubNub pubNub = PubNubClient.getInstance().getPubNub();
 
     private PubNubPublisher() {}
 
-    public static PubNubPublisher getInstance() {
+    public static PubNubPublisher getInstance()
+    {
         if (publisher == null) {
             synchronized (PubNubPublisher.class) {
                 if (publisher == null) {
@@ -19,6 +26,14 @@ public final class PubNubPublisher {
         return publisher;
     }
 
-    public void publish(){}
 
+    /**
+     * Call this method when sending a message to a specified channel
+     * @param message Can be any Object that you would like to send
+     * @param channel destination of the message
+     */
+    public void publish(Object message, String channel)
+    {
+        pubNub.publish().message(message).channel(channel);
+    }
 }
