67f9943abd4aa945586ac7e9d75c8a0520bc46d4
Tucker Harvey <coopert.harvey@gmail.com>
2020-07-06 15:51:23 -0700

    define observer interfaces

--- src/com/jtaodyssey/namespace/notification/JTANotificationObserver.java
+++ src/com/jtaodyssey/namespace/notification/JTANotificationObserver.java
@@ -1,8 +1,11 @@
 package com.jtaodyssey.namespace.notification;
 
+import javax.management.Notification;
+
 /**
  * Provides the interface for concrete observers who wish to be alerted
  * during certain types of notifications
  */
 public interface JTANotificationObserver {
+    void update(Notification notification);
 }
--- src/com/jtaodyssey/namespace/notification/JTANotificationSubject.java
+++ src/com/jtaodyssey/namespace/notification/JTANotificationSubject.java
@@ -1,8 +1,13 @@
 package com.jtaodyssey.namespace.notification;
 
+import javax.management.Notification;
+
 /**
  * Provides the interface for concrete subjects who will publish different
  * types of notifications
  */
 public interface JTANotificationSubject {
+    void addObserver(JTANotificationObserver o);
+    void removeObserver(JTANotificationObserver o);
+    void notify(Notification notification);
 }
