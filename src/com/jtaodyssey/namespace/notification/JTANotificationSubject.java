52237a115d87f5e214a12075d3b53f586c4f5e14
Tucker Harvey <coopert.harvey@gmail.com>
2020-07-06 16:15:29 -0700

    define structure for observer pattern

--- src/com/jtaodyssey/namespace/notification/JTANotificationSubject.java
+++ src/com/jtaodyssey/namespace/notification/JTANotificationSubject.java
@@ -1,13 +1,26 @@
 package com.jtaodyssey.namespace.notification;
 
-import javax.management.Notification;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Provides the interface for concrete subjects who will publish different
  * types of notifications
  */
-public interface JTANotificationSubject {
-    void addObserver(JTANotificationObserver o);
-    void removeObserver(JTANotificationObserver o);
-    void notify(JTANotification notification);
+public abstract class JTANotificationSubject implements JTASubject {
+    private List<JTANotificationObserver> observers;
+
+    public JTANotificationSubject() {
+        observers = new ArrayList<>();
+    }
+
+    public void addObserver(JTANotificationObserver o) {
+        observers.add(o);
+    }
+
+    public void removeObserver(JTANotificationObserver o) {
+        observers.remove(o);
+    }
+
+    public abstract void notify(JTANotification notification);
 }
