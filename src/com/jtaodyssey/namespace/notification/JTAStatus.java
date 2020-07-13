a22d9d741a1a8ee6843aa4fe55d310c214efbcf3
Tucker Harvey <coopert.harvey@gmail.com>
2020-07-13 16:22:44 -0700

    status interface

---
+++ src/com/jtaodyssey/namespace/notification/JTAStatus.java
@@ -0,0 +1,9 @@
+package com.jtaodyssey.namespace.notification;
+
+/**
+ * This class will represent the different types of statuses that occur
+ * as either errors or other information as a result of notification passing
+ */
+public interface JTAStatus {
+    String getStatus();
+}
