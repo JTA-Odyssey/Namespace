543cf516237dda642604ff8f97aed45e462f6e3c
Tucker Harvey <coopert.harvey@gmail.com>
2020-07-13 17:50:37 -0700

    filewrite creates the directory structure if it doesnt exist

--- src/com/jtaodyssey/namespace/database/JTADataWriter.java
+++ src/com/jtaodyssey/namespace/database/JTADataWriter.java
@@ -1,12 +1,36 @@
 package com.jtaodyssey.namespace.database;
 
+import com.jtaodyssey.namespace.components.JTAUser;
+
+import java.io.*;
+import java.nio.file.InvalidPathException;
+import java.util.Properties;
+
 /**
  * This class will save data to the appropriate location
  */
 public class JTADataWriter {
     private volatile static JTADataWriter writer = null;
+    private String userDataPath;
 
-    private JTADataWriter() {}
+    private void initialize() {
+        Properties appProp = new Properties();
+        String location = "config.properties";
+        try {
+            appProp.load(new BufferedInputStream(new FileInputStream(location)));
+            this.userDataPath = appProp.getProperty("userDataPath");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private JTADataWriter() {
+        initialize();
+        File root = new File(userDataPath);
+        if (!root.isDirectory()) {
+            root.mkdir();
+        }
+    }
 
     public static JTADataWriter getInstance() {
         if (writer == null) {
@@ -17,5 +41,9 @@ public class JTADataWriter {
             }
         }
         return writer;
+    }
+
+    public void save(JTAUser user) {
+        // check if the user has a folder
     }
 }
