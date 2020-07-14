ad78271ada8ac880d5c566c08d939b91eecda4d3
Tucker Harvey <coopert.harvey@gmail.com>
2020-07-14 15:47:50 -0700

    init messengingChannel class

---
+++ src/com/jtaodyssey/namespace/components/MessengingChannel.java
@@ -0,0 +1,14 @@
+package com.jtaodyssey.namespace.components;
+
+public class MessengingChannel implements JTAChannel {
+    
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
