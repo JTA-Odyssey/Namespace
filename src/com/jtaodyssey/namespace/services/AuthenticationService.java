944bde751a6a1ff651fe43c380429e2761aa3428
Tucker Harvey <coopert.harvey@gmail.com>
2020-07-15 09:55:54 -0700

    auth service holds a db manager

--- src/com/jtaodyssey/namespace/services/AuthenticationService.java
+++ src/com/jtaodyssey/namespace/services/AuthenticationService.java
@@ -1,14 +1,19 @@
 package com.jtaodyssey.namespace.services;
 
 import com.jtaodyssey.namespace.components.JTALogin;
+import com.jtaodyssey.namespace.database.DBManager;
+import com.jtaodyssey.namespace.database.SQLDatabase;
 
 /**
  * This class will handle all authentication related actions
  */
 public final class AuthenticationService implements JTAAuthenticator {
     private static volatile AuthenticationService auth = null;
+    private DBManager manager;
 
-    private AuthenticationService() {}
+    private AuthenticationService() {
+        this.manager = SQLDatabase.getInstance();
+    }
 
     public static AuthenticationService getInstance() {
         if (auth == null) {
