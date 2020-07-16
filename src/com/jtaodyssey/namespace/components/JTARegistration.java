11f86dc278467b48a94dcb0a56ce62784b416c9a
Tucker Harvey <coopert.harvey@gmail.com>
2020-07-16 13:16:58 -0700

    init jta reg interface

---
+++ src/com/jtaodyssey/namespace/components/JTARegistration.java
@@ -0,0 +1,11 @@
+package com.jtaodyssey.namespace.components;
+
+/**
+ * This interface defines behavior of concrete registration types
+ */
+public interface JTARegistration {
+    String getFirstName();
+    String getLastName();
+    String getUsername();
+    String getPassword();
+}
