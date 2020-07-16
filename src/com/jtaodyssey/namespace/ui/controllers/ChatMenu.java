0eaa75053428cfaedcac8b679b9b832fcf144088
Jeff Adams <jeffreyadams2566@gmail.com>
2020-07-15 17:20:56 -0700

    Added new UI Features to the chatMenu

--- src/com/jtaodyssey/namespace/ui/controllers/ChatMenu.java
+++ src/com/jtaodyssey/namespace/ui/controllers/ChatMenu.java
@@ -1,11 +1,15 @@
 package com.jtaodyssey.namespace.ui.controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
 import com.jtaodyssey.namespace.components.JTATextMessage;
 import com.jtaodyssey.namespace.notification.*;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
@@ -24,14 +28,28 @@ import java.util.ResourceBundle;
 
 public class ChatMenu implements Initializable, JTANotificationObserver
 {
+    // *************
+    // * Button(s) *
+    // *************
+
+    private JFXButton createNewMessageButton;
+
     // ****************
     // * Container(s) *
     // ****************
 
     @FXML
     private ScrollPane chatScrollPane;
+
     @FXML
     private VBox chatBox;
+
+    @FXML
+    private ScrollPane channelScrollPane;
+
+    @FXML
+    private VBox channelBox;
+
 
     // ****************
     // * Text Area(s) *
@@ -56,6 +74,7 @@ public class ChatMenu implements Initializable, JTANotificationObserver
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
+        channelScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         chatArea.setOnKeyPressed(keyEvent ->
         {
             if(keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.CANCEL)
@@ -136,7 +155,7 @@ public class ChatMenu implements Initializable, JTANotificationObserver
         }
         catch (Exception e)
         {
-            String path= new File("com/jtaodyssey/namespace/ui/images/test3.png").toURI().toString();
+            String path = new File("com/jtaodyssey/namespace/ui/images/test3.png").toURI().toString();
             img.setFill(new ImagePattern(new Image(path)));
 
             e.printStackTrace();
@@ -163,15 +182,61 @@ public class ChatMenu implements Initializable, JTANotificationObserver
 //            textMessageBox.getChildren().add(img);
 //        }
 
-        tempFlow.getStyleClass().add("tempFlowSender");
-        flow.getStyleClass().add("textFlowSender");
-        textMessageBox.setAlignment(Pos.BOTTOM_RIGHT);
-        textMessageBox.getChildren().add(flow);
-        textMessageBox.getChildren().add(img);
+        if(text.equals("Hello") || text.equals("What's up"))
+        {
+            tempFlow.getStyleClass().add("tempFlowReceiver");
+            flow.getStyleClass().add("textFlowReceiver");
+            chatBox.setAlignment(Pos.TOP_LEFT);
+            textMessageBox.setAlignment(Pos.CENTER_LEFT);
+            textMessageBox.getChildren().add(img);
+            textMessageBox.getChildren().add(flow);
+        }
+        else
+        {
+            tempFlow.getStyleClass().add("tempFlowSender");
+            flow.getStyleClass().add("textFlowSender");
+            textMessageBox.setAlignment(Pos.BOTTOM_RIGHT);
+            textMessageBox.getChildren().add(flow);
+            textMessageBox.getChildren().add(img);
+        }
+
 
         textMessageBox.getStyleClass().add("textMessageBox");
 
         chatBox.getChildren().addAll(textMessageBox);
+    }
+
+    @FXML
+    public void createNewMessageOnClick()
+    {
+        Label channel = new Label("Test");
+        channel.setMinHeight(36);
+        channel.setMinWidth(130);
+        channel.setPadding(new Insets(0,0,0,5));
+        channel.getStyleClass().add("channelLabel");
+
+
+        // Creating Profile Picture Icon
+        Circle img = new Circle(30, 30, 15);
+
+        HBox channelHBox = new HBox();
+        channelHBox.setAlignment(Pos.CENTER_LEFT);
+
+        try
+        {
+            String path = new File(String.format("/Users/jeffreyadams/Desktop/Namespace/src/com/jtaodyssey/namespace/ui/images/testProfilePicture.jpeg")).toURI().toString();
+            img.setFill(new ImagePattern(new Image(path)));
+        }
+        catch(Exception e)
+        {
+            String path = new File("Namespace/src/com/jtaodyssey/namespace/ui/images/test3.png").toURI().toString();
+            img.setFill(new ImagePattern(new Image(path)));
+        }
+
+        channelHBox.getChildren().add(img);
+        channelHBox.getChildren().add(channel);
+        channelBox.getChildren().add(channelHBox);
+
     }
 
 }
