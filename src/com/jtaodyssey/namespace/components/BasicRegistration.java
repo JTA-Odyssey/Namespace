d18a25e3e07bdbd522390de4d1f9229cfd753dcc
Tucker Harvey <coopert.harvey@gmail.com>
2020-07-16 13:20:28 -0700

    init basic reg class

---
+++ src/com/jtaodyssey/namespace/components/BasicRegistration.java
@@ -0,0 +1,25 @@
+package com.jtaodyssey.namespace.components;
+
+public class BasicRegistration implements JTARegistration {
+    
+
+    @Override
+    public String getFirstName() {
+        return null;
+    }
+
+    @Override
+    public String getLastName() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+}
