d3c0ccf1a989a2aa22ba92af190dd4650e3590b8
Tucker Harvey <coopert.harvey@gmail.com>
2020-07-16 13:25:53 -0700

    reg class defined

--- src/com/jtaodyssey/namespace/components/BasicRegistration.java
+++ src/com/jtaodyssey/namespace/components/BasicRegistration.java
@@ -1,25 +1,27 @@
 package com.jtaodyssey.namespace.components;
 
 public class BasicRegistration implements JTARegistration {
-    
+    private JTAUser user;
+    private JTALogin login;
 
-    @Override
-    public String getFirstName() {
-        return null;
+    public BasicRegistration(String fn, String ln, String username, String password) {
+        this(new BasicUser(fn, ln), new JTALogin(username, password));
     }
 
-    @Override
-    public String getLastName() {
-        return null;
+    public BasicRegistration(JTAUser user, JTALogin login) {
+        setUser(user);
+        setLogin(login);
     }
 
-    @Override
-    public String getUsername() {
-        return null;
-    }
+    public void setUser(JTAUser user) { this.user = user; }
+    public void setLogin(JTALogin login) { this.login = login; }
 
     @Override
-    public String getPassword() {
-        return null;
-    }
+    public String getFirstName() { return user.getFirstName(); }
+    @Override
+    public String getLastName() { return user.getLastName(); }
+    @Override
+    public String getUsername() { return login.getUsername(); }
+    @Override
+    public String getPassword() { return login.getPassword(); }
 }
