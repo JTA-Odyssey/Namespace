8040fd386a0096caceddb79bff443a02846c628e
Tucker Harvey <coopert.harvey@gmail.com>
2020-07-19 15:25:17 -0700

    init Notification Factory

---
+++ src/com/jtaodyssey/namespace/notification/NotificationFactory.java
@@ -0,0 +1,24 @@
+package com.jtaodyssey.namespace.notification;
+
+/**
+ * this factory is used to create the different types of notifications
+ *
+ * type:
+ *  (1) "add-contact" -> addContactNotification
+ *  (2) "lookup-contact" -> lookupContactNotification
+ *  (3) "remove-contact" -> removeContactNotification
+ */
+public class NotificationFactory {
+    public JTANotification createNotification(String type, Payload payload) {
+        if (type.toLowerCase().equals("add-contact")) {
+
+        }
+        else if (type.toLowerCase().equals("lookup-contact")) {
+
+        } 
+        else if (type.toLowerCase().equals("remove-contact")) {
+
+        }
+        return null;
+    }
+}
