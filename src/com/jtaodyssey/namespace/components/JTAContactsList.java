282ca081606f14f56f8c737edac646bfe16455a9
Tucker Harvey <coopert.harvey@gmail.com>
2020-07-19 14:50:16 -0700

    init contacts list

---
+++ src/com/jtaodyssey/namespace/components/JTAContactsList.java
@@ -0,0 +1,12 @@
+package com.jtaodyssey.namespace.components;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * This class will be used to manage the contacts of a user
+ */
+public class JTAContactsList implements Serializable {
+    private List<JTAContact> contacts;
+    
+}
